6c35ee407b09e7a735931a1a5db1aa4bafad0336
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-07-05 14:18:23 -0700

    Add GLFW and glad

---
+++ source/GLIncludes.h
@@ -0,0 +1,10 @@
+#ifndef GLINCLUDES_H
+#define GLINCLUDES_H
+
+#include <glad/glad.h>
+
+#define GLFW_INCLUDE_NONE
+#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
+#endif
--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,65 @@
 #include "Constants.h"
+#include "GLIncludes.h"
 #include "LogHelper.h"
 
 #include <cstdlib>
 
+namespace {
+
+GLFWwindow* init() {
+   int glfwInitRes = glfwInit();
+   if (!glfwInitRes) {
+      LOG_ERROR("Unable to initialize GLFW");
+      return nullptr;
+   }
+
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+   GLFWwindow *window = glfwCreateWindow(160, 144, PROJECT_DISPLAY_NAME, nullptr, nullptr);
+   if (!window) {
+      glfwTerminate();
+      LOG_ERROR("Unable to create GLFW window");
+      return nullptr;
+   }
+
+   glfwMakeContextCurrent(window);
+   glfwSwapInterval(1); // VSYNC
+
+   int gladInitRes = gladLoadGL();
+   if (!gladInitRes) {
+      glfwDestroyWindow(window);
+      glfwTerminate();
+      LOG_ERROR("Unable to initialize glad");
+      return nullptr;
+   }
+
+   return window;
+}
+
+} // namespace
+
 int main(int argc, char *argv[]) {
    LOG_INFO(PROJECT_NAME << " " << VERSION_TYPE << " " << VERSION_MAJOR << "." << VERSION_MINOR << "."
       << VERSION_MICRO << "." << VERSION_BUILD);
+
+   GLFWwindow *window = init();
+   if (!window) {
+      return EXIT_FAILURE;
+   }
+
+   while (!glfwWindowShouldClose(window)) {
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      glfwSwapBuffers(window);
+
+      glfwPollEvents();
+   }
+
+   glfwDestroyWindow(window);
+   glfwTerminate();
 
    return EXIT_SUCCESS;
 }
