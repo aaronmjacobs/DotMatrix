4af63fbaf5720b9db4ec8bb04da4f1b0d05617b8
Aaron Jacobs <aaron@aaronmjacobs.com>
2016-07-11 00:27:05 -0700

    Implement I/O registers

--- source/gbc/Memory.h
+++ source/gbc/Memory.h
@@ -20,18 +20,69 @@ union Memory {
 
       union {                 // I/O device mappings          (0xFF00-0xFF7F, 128)
          uint8_t io[0x80];    // Direct access                (0xFF00-0xFF7F, 128)
+
          struct {
+            // General
             uint8_t p1;       // Joy pad / system info        (0xFF00, 1)
             uint8_t sb;       // Serial transfer data         (0xFF01, 1)
-            uint8_t sc;       // IO Control                   (0xFF02, 1)
+            uint8_t sc;       // IO control                   (0xFF02, 1)
             uint8_t pad0[1];  // Padding                      (0xFF03, 1)
             uint8_t div;      // Divider register             (0xFF04, 1)
             uint8_t tima;     // Timer counter                (0xFF05, 1)
-            uint8_t tma;      // Timer Modulo                 (0xFF06, 1)
-            uint8_t tac;      // Timer Control                (0xFF07, 1)
+            uint8_t tma;      // Timer modulo                 (0xFF06, 1)
+            uint8_t tac;      // Timer control                (0xFF07, 1)
             uint8_t pad1[7];  // Padding                      (0xFF08-0xFF0E, 7)
-            uint8_t iff;      // Interrupt Flag               (0xFF0F, 1)
-            uint8_t pad2[112];// Padding                      (0xFF10-0xFF7F, 112)
+            uint8_t ifr;      // Interrupt flag               (0xFF0F, 1)
+
+            // Sound mode 1
+            uint8_t nr10;     // Sweep                        (0xFF10, 1)
+            uint8_t nr11;     // Length / wave pattern duty   (0xFF11, 1)
+            uint8_t nr12;     // Envelope                     (0xFF12, 1)
+            uint8_t nr13;     // Frequency lo                 (0xFF13, 1)
+            uint8_t nr14;     // Frequency hi                 (0xFF14, 1)
+            uint8_t pad2[1];  // Padding                      (0xFF15, 1)
+
+            // Sound mode 2
+            uint8_t nr21;     // Length / wave pattern duty   (0xFF16, 1)
+            uint8_t nr22;     // Envelope                     (0xFF17, 1)
+            uint8_t nr23;     // Frequency lo                 (0xFF18, 1)
+            uint8_t nr24;     // Frequency hi                 (0xFF19, 1)
+
+            // Sound mode 3
+            uint8_t nr30;     // On / off                     (0xFF1A, 1)
+            uint8_t nr31;     // Length                       (0xFF1B, 1)
+            uint8_t nr32;     // Output level                 (0xFF1C, 1)
+            uint8_t nr33;     // Frequency lo                 (0xFF1D, 1)
+            uint8_t nr34;     // Frequency hi                 (0xFF1E, 1)
+            uint8_t pad3[1];  // Padding                      (0xFF1F, 1)
+
+            // Sound mode 4
+            uint8_t nr41;     // Length                       (0xFF20, 1)
+            uint8_t nr42;     // Envelope                     (0xFF21, 1)
+            uint8_t nr43;     // Polynomial counter           (0xFF22, 1)
+            uint8_t nr44;     // Counter/consecutive; initial (0xFF23, 1)
+
+            // Sound general
+            uint8_t nr50;     // Channel control/on-off/vol.  (0xFF24, 1)
+            uint8_t nr51;     // Output terminal selection    (0xFF25, 1)
+            uint8_t nr52;     // On / off                     (0xFF26, 1)
+            uint8_t pad4[9];  // Padding                      (0xFF27-0xFF2F, 9)
+            uint8_t wave[16]; // Wave pattern RAM             (0xFF30-0xFF3F, 16)
+
+            // LCD
+            uint8_t lcdc;     // LCD control                  (0xFF40, 1)
+            uint8_t stat;     // LCD status                   (0xFF41, 1)
+            uint8_t scy;      // Scroll y                     (0xFF42, 1)
+            uint8_t scx;      // Scroll x                     (0xFF43, 1)
+            uint8_t ly;       // LCDC y-coordinate            (0xFF44, 1)
+            uint8_t lyc;      // LY compare                   (0xFF45, 1)
+            uint8_t dma;      // DMA transfer & start address (0xFF46, 1)
+            uint8_t bgp;      // BG & window palette data     (0xFF47, 1)
+            uint8_t obp0;     // Object palette 0 data        (0xFF48, 1)
+            uint8_t obp1;     // Object palette 1 data        (0xFF49, 1)
+            uint8_t wy;       // Window y position            (0xFF4A, 1)
+            uint8_t wx;       // Window x position            (0xFF4B, 1)
+            uint8_t pad5[52]; // Padding                      (0xFF4C-0xFF7F, 52)
          };
       };
 
