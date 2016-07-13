b1a915fdd54635f804b6e9e157ad67db9886b9c7
Aaron Jacobs <aaron@aaronmjacobs.com>
2016-07-13 00:25:41 -0700

    Update GLFW to 3.2

--- source/GLIncludes.h
+++ source/GLIncludes.h
@@ -4,7 +4,6 @@
 #include <glad/glad.h>
 
 #define GLFW_INCLUDE_NONE
-#define GLFW_DLL
 #include <GLFW/glfw3.h>
 
 #endif
--- source/main.cpp
+++ source/main.cpp
@@ -31,7 +31,7 @@ GLFWwindow* init() {
    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
    GLFWwindow *window = glfwCreateWindow(GBC::kScreenWidth, GBC::kScreenHeight, kProjectDisplayName, nullptr, nullptr);
    if (!window) {
