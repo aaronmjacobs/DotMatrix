503c468a763ae69a9c730159472e4b9f3fcc91f5
Aaron Jacobs <aaron@aaronmjacobs.com>
2020-09-28 00:27:34 -0700

    Update CMake scripts to support multiple targets

--- Libraries.cmake
+++ Libraries.cmake
@@ -1,18 +1,56 @@
 set(LIB_DIR "${PROJECT_SOURCE_DIR}/Libraries")
+set(COPY_LIBS)
+
+## Integrated ##
+
+# imgui
+if(DOT_MATRIX_WITH_UI)
+   set(IMGUI_DIR "${LIB_DIR}/imgui")
+   set(IMGUI_SOURCES
+      "${IMGUI_DIR}/imconfig.h"
+      "${IMGUI_DIR}/imgui.cpp"
+      "${IMGUI_DIR}/imgui.h"
+      "${IMGUI_DIR}/imgui_demo.cpp"
+      "${IMGUI_DIR}/imgui_draw.cpp"
+      "${IMGUI_DIR}/imgui_internal.h"
+      "${IMGUI_DIR}/imgui_widgets.cpp"
+      "${IMGUI_DIR}/imstb_rectpack.h"
+      "${IMGUI_DIR}/imstb_textedit.h"
+      "${IMGUI_DIR}/imstb_truetype.h"
+      "${IMGUI_DIR}/examples/imgui_impl_glfw.h"
+      "${IMGUI_DIR}/examples/imgui_impl_glfw.cpp"
+      "${IMGUI_DIR}/examples/imgui_impl_opengl3.h"
+      "${IMGUI_DIR}/examples/imgui_impl_opengl3.cpp"
+   )
+   target_sources(${PROJECT_NAME} PRIVATE "${IMGUI_SOURCES}")
+   target_include_directories(${PROJECT_NAME} PRIVATE "${IMGUI_DIR}")
+   target_compile_definitions(${PROJECT_NAME} PRIVATE IMGUI_IMPL_OPENGL_LOADER_GLAD IMGUI_USER_CONFIG="UI/UIConfig.h")
+
+   set(IMGUI_CLUB_DIR "${LIB_DIR}/imgui_club")
+   target_sources(${PROJECT_NAME} PRIVATE "${IMGUI_CLUB_DIR}/imgui_memory_editor/imgui_memory_editor.h")
+   target_include_directories(${PROJECT_NAME} PRIVATE "${IMGUI_CLUB_DIR}")
+
+   source_group("Libraries\\imgui_club\\imgui_memory_editor" "${IMGUI_CLUB_DIR}/imgui_memory_editor/*") # This needs to come first
+   source_group("Libraries\\imgui" "${IMGUI_DIR}/*")
+endif()
 
 # libretro
-if(${DOT_MATRIX_WITH_LIBRETRO})
-	set(LIBRETRO_DIR "${LIB_DIR}/libretro")
-	target_sources(${PROJECT_NAME} PRIVATE "${LIBRETRO_DIR}/libretro.h")
-	target_include_directories(${PROJECT_NAME} PUBLIC "${LIBRETRO_DIR}")
-	source_group("Libraries\\libretro" "${LIBRETRO_DIR}")
-endif()
+set(LIBRETRO_DIR "${LIB_DIR}/libretro")
+target_sources(${RETRO_PROJECT_NAME} PRIVATE "${LIBRETRO_DIR}/libretro.h")
+target_include_directories(${RETRO_PROJECT_NAME} PRIVATE "${LIBRETRO_DIR}")
+source_group("Libraries\\libretro" "${LIBRETRO_DIR}")
 
 # PPK_ASSERT
 set(PPK_DIR "${LIB_DIR}/PPK_ASSERT")
-target_sources(${PROJECT_NAME} PRIVATE "${PPK_DIR}/src/ppk_assert.h;${PPK_DIR}/src/ppk_assert.cpp")
-target_include_directories(${PROJECT_NAME} PUBLIC "${PPK_DIR}/src")
+target_sources(${COMMON_PROJECT_NAME} INTERFACE "${PPK_DIR}/src/ppk_assert.h;${PPK_DIR}/src/ppk_assert.cpp")
+target_include_directories(${COMMON_PROJECT_NAME} INTERFACE "${PPK_DIR}/src")
 source_group("Libraries\\PPK_ASSERT" "${PPK_DIR}/src")
+
+# readerwriterqueue
+set(QUEUE_DIR "${LIB_DIR}/readerwriterqueue")
+target_sources(${EXECUTABLE_COMMON_PROJECT_NAME} INTERFACE "${QUEUE_DIR}/atomicops.h;${QUEUE_DIR}/readerwriterqueue.h")
+target_include_directories(${EXECUTABLE_COMMON_PROJECT_NAME} INTERFACE "${QUEUE_DIR}")
+source_group("Libraries\\readerwriterqueue" "${QUEUE_DIR}/*")
 
 # templog
 set(TEMPLOG_DIR "${LIB_DIR}/templog")
@@ -24,6 +62,70 @@ set(TEMPLOG_SOURCES
    "${TEMPLOG_DIR}/type_lists.h"
    "${TEMPLOG_DIR}/imp/logging.cpp"
 )
-target_sources(${PROJECT_NAME} PRIVATE "${TEMPLOG_SOURCES}")
-target_include_directories(${PROJECT_NAME} PUBLIC "${TEMPLOG_DIR}")
+target_sources(${COMMON_PROJECT_NAME} INTERFACE "${TEMPLOG_SOURCES}")
+target_include_directories(${COMMON_PROJECT_NAME} INTERFACE "${TEMPLOG_DIR}")
 source_group("Libraries\\templog" "${TEMPLOG_DIR}/*")
+
+## Static ##
+
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build shared libraries")
+
+# Boxer
+add_subdirectory("${LIB_DIR}/Boxer")
+target_link_libraries(${PROJECT_NAME} PRIVATE Boxer)
+
+# glad
+add_subdirectory("${LIB_DIR}/glad")
+target_link_libraries(${PROJECT_NAME} PRIVATE optimized glad)
+target_link_libraries(${PROJECT_NAME} PRIVATE debug glad-debug)
+list(APPEND COPY_LIBS "${CMAKE_DL_LIBS}")
+
+# PlatformUtils
+add_subdirectory("${LIB_DIR}/PlatformUtils")
+target_link_libraries(${EXECUTABLE_COMMON_PROJECT_NAME} INTERFACE PlatformUtils)
+
+## Shared ##
+
+set(BUILD_SHARED_LIBS ON CACHE INTERNAL "Build shared libraries")
+
+# GLFW
+set(GLFW_BUILD_EXAMPLES OFF CACHE INTERNAL "Build the GLFW example programs")
+set(GLFW_BUILD_TESTS OFF CACHE INTERNAL "Build the GLFW test programs")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build the GLFW documentation")
+set(GLFW_INSTALL OFF CACHE INTERNAL "Generate installation target")
+add_subdirectory("${LIB_DIR}/glfw")
+target_compile_definitions(${PROJECT_NAME} PRIVATE GLFW_INCLUDE_NONE)
+target_link_libraries(${PROJECT_NAME} PRIVATE glfw)
+list(APPEND COPY_LIBS glfw)
+
+# OpenAL
+if(DOT_MATRIX_WITH_AUDIO)
+   set(OPENAL_DIR "${LIB_DIR}/openal-soft")
+   set(ALSOFT_UTILS OFF CACHE INTERNAL "Build and install utility programs")
+   set(ALSOFT_NO_CONFIG_UTIL ON CACHE INTERNAL "Disable building the alsoft-config utility")
+   set(ALSOFT_EXAMPLES OFF CACHE INTERNAL "Build and install example programs")
+   set(ALSOFT_INSTALL OFF CACHE INTERNAL "Install headers and libraries")
+   set(ALSOFT_INSTALL_CONFIG OFF CACHE INTERNAL "Install alsoft.conf sample configuration file")
+   set(ALSOFT_INSTALL_HRTF_DEFS OFF CACHE INTERNAL "Install HRTF definition files")
+   set(ALSOFT_INSTALL_AMBDEC_PRESETS OFF CACHE INTERNAL "Install AmbDec preset files")
+   set(ALSOFT_INSTALL_EXAMPLES OFF CACHE INTERNAL "Install example programs (alplay, alstream, ...)")
+   set(ALSOFT_INSTALL_UTILS OFF CACHE INTERNAL "Install utility programs (openal-info, alsoft-config, ...)")
+   set(ALSOFT_UPDATE_BUILD_VERSION OFF CACHE INTERNAL "Update git build version info")
+   add_subdirectory("${LIB_DIR}/openal-soft")
+   target_link_libraries(${PROJECT_NAME} PRIVATE OpenAL)
+   list(APPEND COPY_LIBS OpenAL)
+endif()
+
+## Post-Build ##
+
+# Copy DLLs
+if(WIN32)
+   foreach(COPY_LIB ${COPY_LIBS})
+      add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+         COMMAND ${CMAKE_COMMAND} -E copy_if_different "$<TARGET_FILE:${COPY_LIB}>" "$<TARGET_FILE_DIR:${PROJECT_NAME}>"
+      )
+      add_custom_command(TARGET ${TEST_PROJECT_NAME} POST_BUILD
+         COMMAND ${CMAKE_COMMAND} -E copy_if_different "$<TARGET_FILE:${COPY_LIB}>" "$<TARGET_FILE_DIR:${TEST_PROJECT_NAME}>"
+      )
+   endforeach()
+endif()
--- Source.cmake
+++ Source.cmake
@@ -1,37 +1,156 @@
 set(SRC_DIR "${PROJECT_SOURCE_DIR}/Source")
-set(CORE_SRC_DIR "${SRC_DIR}/${PROJECT_NAME}")
 
-target_include_directories(${PROJECT_NAME} PUBLIC "${SRC_DIR}")
+# Files
 
-target_sources(${PROJECT_NAME} PRIVATE
-   "${CORE_SRC_DIR}/Core/Archive.h"
-   "${CORE_SRC_DIR}/Core/Assert.h"
-   "${CORE_SRC_DIR}/Core/Enum.h"
-   "${CORE_SRC_DIR}/Core/Log.h"
-   "${CORE_SRC_DIR}/Core/Log.cpp"
-   "${CORE_SRC_DIR}/Core/Math.h"
-
-   "${CORE_SRC_DIR}/GameBoy/Cartridge.h"
-   "${CORE_SRC_DIR}/GameBoy/Cartridge.cpp"
-   "${CORE_SRC_DIR}/GameBoy/CPU.h"
-   "${CORE_SRC_DIR}/GameBoy/CPU.cpp"
-   "${CORE_SRC_DIR}/GameBoy/GameBoy.h"
-   "${CORE_SRC_DIR}/GameBoy/GameBoy.cpp"
-   "${CORE_SRC_DIR}/GameBoy/LCDController.h"
-   "${CORE_SRC_DIR}/GameBoy/LCDController.cpp"
-   "${CORE_SRC_DIR}/GameBoy/MemoryBankController.h"
-   "${CORE_SRC_DIR}/GameBoy/MemoryBankController.cpp"
-   "${CORE_SRC_DIR}/GameBoy/Operations.h"
-   "${CORE_SRC_DIR}/GameBoy/Operations.cpp"
-   "${CORE_SRC_DIR}/GameBoy/SoundController.h"
-   "${CORE_SRC_DIR}/GameBoy/SoundController.cpp"
+set(CORE_SOURCE_FILES
+   "${SRC_DIR}/Core/Archive.h"
+   "${SRC_DIR}/Core/Assert.h"
+   "${SRC_DIR}/Core/Enum.h"
+   "${SRC_DIR}/Core/Log.h"
+   "${SRC_DIR}/Core/Log.cpp"
+   "${SRC_DIR}/Core/Math.h"
 )
 
-if(${DOT_MATRIX_WITH_LIBRETRO})
-   target_sources(${PROJECT_NAME} PRIVATE
-      "${SRC_DIR}/Retro/Retro.cpp"
+set(EMULATOR_SOURCE_FILES
+   "${SRC_DIR}/Emulator/Emulator.h"
+   "${SRC_DIR}/Emulator/Emulator.cpp"
+   "${SRC_DIR}/Emulator/Logo.inl"
+   "${SRC_DIR}/Emulator/Main.cpp"
+)
+
+set(GAMEBOY_SOURCE_FILES
+   "${SRC_DIR}/GameBoy/Cartridge.h"
+   "${SRC_DIR}/GameBoy/Cartridge.cpp"
+   "${SRC_DIR}/GameBoy/CPU.h"
+   "${SRC_DIR}/GameBoy/CPU.cpp"
+   "${SRC_DIR}/GameBoy/GameBoy.h"
+   "${SRC_DIR}/GameBoy/GameBoy.cpp"
+   "${SRC_DIR}/GameBoy/LCDController.h"
+   "${SRC_DIR}/GameBoy/LCDController.cpp"
+   "${SRC_DIR}/GameBoy/MemoryBankController.h"
+   "${SRC_DIR}/GameBoy/MemoryBankController.cpp"
+   "${SRC_DIR}/GameBoy/Operations.h"
+   "${SRC_DIR}/GameBoy/Operations.cpp"
+   "${SRC_DIR}/GameBoy/SoundController.h"
+   "${SRC_DIR}/GameBoy/SoundController.cpp"
+)
+
+set(PLATFORM_AUDIO_SOURCE_FILES
+   "${SRC_DIR}/Platform/Audio/AudioManager.h"
+   "${SRC_DIR}/Platform/Audio/AudioManager.cpp"
+)
+
+set(PLATFORM_INPUT_SOURCE_FILES
+   "${SRC_DIR}/Platform/Input/InputDevice.h"
+   "${SRC_DIR}/Platform/Input/ControllerInputDevice.h"
+   "${SRC_DIR}/Platform/Input/ControllerInputDevice.cpp"
+   "${SRC_DIR}/Platform/Input/KeyboardInputDevice.h"
+   "${SRC_DIR}/Platform/Input/KeyboardInputDevice.cpp"
+)
+
+set(PLATFORM_VIDEO_SOURCE_FILES
+   "${SRC_DIR}/Platform/Video/Mesh.h"
+   "${SRC_DIR}/Platform/Video/Mesh.cpp"
+   "${SRC_DIR}/Platform/Video/Model.h"
+   "${SRC_DIR}/Platform/Video/Model.cpp"
+   "${SRC_DIR}/Platform/Video/Renderer.h"
+   "${SRC_DIR}/Platform/Video/Renderer.cpp"
+   "${SRC_DIR}/Platform/Video/Shader.h"
+   "${SRC_DIR}/Platform/Video/Shader.cpp"
+   "${SRC_DIR}/Platform/Video/ShaderProgram.h"
+   "${SRC_DIR}/Platform/Video/ShaderProgram.cpp"
+   "${SRC_DIR}/Platform/Video/Texture.h"
+   "${SRC_DIR}/Platform/Video/Texture.cpp"
+)
+
+set(RETRO_SOURCE_FILES
+   "${SRC_DIR}/Retro/Retro.cpp"
+)
+
+set(TEST_SOURCE_FILES
+   "${SRC_DIR}/Test/TestMain.cpp"
+)
+
+set(UI_SOURCE_FILES
+   "${SRC_DIR}/UI/AfterCoreIncludes.inl"
+   "${SRC_DIR}/UI/BeforeCoreIncludes.inl"
+   "${SRC_DIR}/UI/CartridgeWindow.cpp"
+   "${SRC_DIR}/UI/CPUWindow.cpp"
+   "${SRC_DIR}/UI/EmulatorWindow.cpp"
+   "${SRC_DIR}/UI/JoypadWindow.cpp"
+   "${SRC_DIR}/UI/LCDControllerWindow.cpp"
+   "${SRC_DIR}/UI/MemoryWindow.cpp"
+   "${SRC_DIR}/UI/ScreenWindow.cpp"
+   "${SRC_DIR}/UI/SoundControllerWindow.cpp"
+   "${SRC_DIR}/UI/TimerWindow.cpp"
+   "${SRC_DIR}/UI/UI.h"
+   "${SRC_DIR}/UI/UI.cpp"
+   "${SRC_DIR}/UI/UIConfig.h"
+)
+
+set(UI_DEBUGGER_SOURCE_FILES
+   "${SRC_DIR}/UI/DebuggerWindow.cpp"
+)
+
+# File groups
+
+set(ALL_SOURCE_FILES
+   ${CORE_SOURCE_FILES}
+   ${EMULATOR_SOURCE_FILES}
+   ${GAMEBOY_SOURCE_FILES}
+   ${PLATFORM_AUDIO_SOURCE_FILES}
+   ${PLATFORM_INPUT_SOURCE_FILES}
+   ${PLATFORM_VIDEO_SOURCE_FILES}
+   ${RETRO_SOURCE_FILES}
+   ${TEST_SOURCE_FILES}
+   ${UI_SOURCE_FILES}
+   ${UI_DEBUGGER_SOURCE_FILES}
+)
+
+set(EMULATOR_PROJECT_SOURCE_FILES
+   ${CORE_SOURCE_FILES}
+   ${EMULATOR_SOURCE_FILES}
+   ${GAMEBOY_SOURCE_FILES}
+   ${PLATFORM_INPUT_SOURCE_FILES}
+   ${PLATFORM_VIDEO_SOURCE_FILES}
+)
+
+if(DOT_MATRIX_WITH_AUDIO)
+   list(APPEND EMULATOR_PROJECT_SOURCE_FILES
+      ${PLATFORM_AUDIO_SOURCE_FILES}
    )
 endif()
 
-get_target_property(SOURCE_FILES ${PROJECT_NAME} SOURCES)
-source_group(TREE ${SRC_DIR} PREFIX Source FILES ${SOURCE_FILES})
+if(DOT_MATRIX_WITH_UI)
+   list(APPEND EMULATOR_PROJECT_SOURCE_FILES
+      ${UI_SOURCE_FILES}
+   )
+
+   if (DOT_MATRIX_WITH_DEBUGGER)
+      list(APPEND EMULATOR_PROJECT_SOURCE_FILES
+         ${UI_DEBUGGER_SOURCE_FILES}
+      )
+   endif()
+endif()
+
+set(RETRO_PROJECT_SOURCE_FILES
+   ${CORE_SOURCE_FILES}
+   ${GAMEBOY_SOURCE_FILES}
+   ${RETRO_SOURCE_FILES}
+)
+
+set(TEST_PROJECT_SOURCE_FILES
+   ${CORE_SOURCE_FILES}
+   ${GAMEBOY_SOURCE_FILES}
+   ${TEST_SOURCE_FILES}
+)
+
+# Targets
+
+target_include_directories(${COMMON_PROJECT_NAME} INTERFACE "${SRC_DIR}")
+
+target_sources(${PROJECT_NAME} PRIVATE ${EMULATOR_PROJECT_SOURCE_FILES})
+target_sources(${RETRO_PROJECT_NAME} PRIVATE ${RETRO_PROJECT_SOURCE_FILES})
+target_sources(${TEST_PROJECT_NAME} PRIVATE ${TEST_PROJECT_SOURCE_FILES})
+
+source_group(TREE ${SRC_DIR} PREFIX Source FILES ${ALL_SOURCE_FILES})
